19ac57ff0e105003d641d1689a344b86489759ff
Filipe Borges <filipebkc2209@gmail.com>
2020-06-08 21:22:18 -0300

    Initial commit

---
+++ src/main/java/br/com/filipeborges/resillience/resillience4jexample/Resillience4jExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.filipeborges.resillience.resillience4jexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resillience4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resillience4jExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/filipeborges/resillience/resillience4jexample/Resillience4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filipeborges.resillience.resillience4jexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resillience4jExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
